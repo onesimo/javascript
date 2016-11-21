caa00ffbb07cf4c049e8a68f4b44170afb657d49
Onesimo Batista <onesimobatista@gmail.com>
2016-11-21 20:06:37 -0200

    polymorphism

---
+++ oop/abstract_class.js
@@ -0,0 +1,27 @@
+var Animal = function() {
+	throw "This is an abstract class";
+}
+
+Animal.prototype.name = "";
+Animal.prototype.sound = "";
+Animal.prototype.say = function () {
+	return console.log(this.name+ "Emit -> "+this.sound);
+};
+
+var Cat = function() {
+	this.name  = 'Cat';
+	this.sound = 'meow';
+};
+
+var Dog = function() {
+	this.name = 'Dog';
+	this.sound = 'woof';
+};
+
+Cat.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+console.log(new Cat());
+
+
+
