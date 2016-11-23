cc62f595af5604febccaf77e0dff843a8e2645a7
Onesimo Batista <onesimobatista@gmail.com>
2016-11-22 22:13:18 -0200

    validate form

--- advanced/js/main.js
+++ advanced/js/main.js
@@ -17,7 +17,7 @@ function setList(list){
 	var table = '<thead><tr><td>Description</td> <td>Amount</td> <td>Value</td> <td>Action</td></tr></thead><tbody>';
 
 	for(var key in list){
-		table += '<tr><td>'+formatDesc(list[key].desc)+'</td><td>'+list[key].amount+'</td><td>'+formatValue(list[key].value)+'</td><td><button class="btn btn-default" onclick="setUpdate('+key+')">Edit</button> | <button class="btn btn-default" onclick="deleteData('+key+')">Delete</button></td></tr>'; 		
+		table += '<tr><td>'+formatDesc(list[key].desc)+'</td><td>'+formatAmout(list[key].amount)+'</td><td>'+formatValue(list[key].value)+'</td><td><button class="btn btn-default" onclick="setUpdate('+key+')">Edit</button> | <button class="btn btn-default" onclick="deleteData('+key+')">Delete</button></td></tr>'; 		
 	}
 
 	table += '</tbody>';
@@ -30,6 +30,10 @@ function formatDesc(desc){
 	return str;
 }
 
+function formatAmout(amount){
+	return parseInt(amount)
+}
+
 function formatValue(value){
 	var str = parseFloat(value).toFixed(2) + "";
 	str = str.replace(".",",");
@@ -38,6 +42,9 @@ function formatValue(value){
 }
 
 function addData(){
+	if(!validate()){
+		return;
+	}
 	var desc = document.getElementById('desc').value;
 	var amount = document.getElementById('amount').value;
 	var value = document.getElementById('value').value;
@@ -68,6 +75,9 @@ function resetData(){
 }
 
 function updateData(){
+	if(!validate()){
+		return;
+	}
 	var id = document.getElementById('idUpdate').value;
 	var desc = document.getElementById('desc').value;
 	var amount = document.getElementById('amount').value;
@@ -94,4 +104,43 @@ function deleteData(id){
 		}
 		setList(list);
 	}
+}
+
+function validate(){
+	var desc = document.getElementById('desc').value;
+	var amount = document.getElementById('amount').value;
+	var value = document.getElementById('value').value;
+	var errors = "";
+
+	if(desc === ""){
+		errors += '<p>Fill out description</p>';
+	}
+
+	if(amount === ""){
+		errors += '<p>Fill out quantity</p>';
+	}else if(amount != parseInt(amount)){
+		errors += '<p>Fill out a valid quantity</p>';
+	}
+
+	if(value === ""){
+		errors += '<p>Fill out a value</p>';
+	}else if(value != parseFloat(value)){
+		errors += '<p>Fill out a valid value</p>';		
+	}
+
+
+	if(errors != ""){
+		document.getElementById('errors').style.display = "block";
+		document.getElementById('errors').style.backgroundColor = "rgba(85,85,85,0.3)";
+		document.getElementById('errors').style.color = "white";
+		document.getElementById('errors').style.padding = "10px";
+		document.getElementById('errors').style.margin = "10px";
+		document.getElementById('errors').style.borderRadius = "13px";
+		document.getElementById('errors').innerHTML = "<h3>error</h3>" + errors;
+		return 0;
+	}else{
+		document.getElementById('errors').style.display = "none";
+		return 1;
+	}
+
 }
